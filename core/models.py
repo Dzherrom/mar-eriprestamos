2d2a65c440d1d8303fe4d53f61702d1d2d7ba3fc
Jerome Rojas <skreamlight@gmail.com>
2024-12-02 20:40:04 -0400

    prestamos site

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    numero_telefono = models.CharField(max_length=11)
+    cedula = models.CharField(max_length=12)
+    ciudad = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.nombre
+
+class Prestamos(models.Model):
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_pago = models.DateField(null=True, blank=True)
+    monto_prestamo = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    monto_pago = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    tasa_interes = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return f"Préstamo de {self.cliente} - Monto: {self.monto_prestamo}"
+    
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.http import HttpResponse
+from .models import Prestamos
+#from .forms import
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def prestamos(request):
+    if request.method == 'GET':
+        prestamos = Prestamos.objects.all()
+        context = {'mensaje':'error', 'prestamos': prestamos}
+        return render(request, 'prestamos/prestamos.html', context)
+    
